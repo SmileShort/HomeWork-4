d7269ed1ee40ba5cc9eccdbf93b2f98dcb496608
SmileShort <86470906+SmileShort@users.noreply.github.com>
2021-11-25 00:03:00 +0500

    Initial commit

---
+++ src/main/java/org/example/Triangle.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+
+    int a;
+    int b;
+    int c;
+    double p;
+    double s;
+
+    public Triangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Triangle() {
+    }
+
+
+    public double areaOfTriangle (Triangle o){
+        if (a <= 0 || b <= 0 || c <= 0)
+            System.out.println("В треугольнике должны быть только положительные длины сторон");
+        p = (a + b + c) / 2;
+        s = sqrt(p * (p - a) * (p - b) * (p - c));
+        if (s <= 0)
+            System.out.println("Невозможно подсчитать площать треугольника");
+        if (s > 0)
+            System.out.println("Плащадь треугольника равна: " + s);
+        return s;
+    }
+
+    public double areaOfTriangle (int a, int b, int c){
+        if (a <= 0 || b <= 0 || c <= 0) {
+            System.out.println("В треугольнике должны быть только положительные длины сторон");
+        }
+        p = (a + b + c) / 2;
+        s = sqrt(p * (p - a) * (p - b) * (p - c));
+        if (s <= 0)
+            System.out.println("Невозможно подсчитать площать треугольника");
+        else
+            System.out.println("Плащадь треугольника равна: " + s);
+        return s;
+    }
+
+    public boolean itsTriangle(){
+        return a > 0 && b > 0 && c > 0;
+    }
+
+    public boolean canColculateArea(Triangle obj){
+        if (obj.areaOfTriangle(obj) > 0)
+            return true;
+        else return false;
+    }
+
+}
---
+++ src/test/java/org/example/TriangleTest.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+public class TriangleTest {
+
+    private static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
+
+    @Test
+    public void correctTriangle(){
+        Triangle obj1= new Triangle(3, 4, 5);
+        Assert.assertNotNull(obj1);
+    }
+
+    @Test
+    public void triangleHasWrongSides(){
+        Triangle obj1= new Triangle(1, 2, 2);
+        obj1.areaOfTriangle(obj1);
+        Assert.assertTrue(true);
+
+    }
+
+    @Test
+    public void TriangleHaveNegativeSides(){
+        Triangle obj1= new Triangle(-1, 2, 2);
+        obj1.areaOfTriangle(obj1);
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void itsTriangleTest(){
+        Triangle obj1= new Triangle(3, 4, 5);
+        Assert.assertEquals(true, obj1.itsTriangle());
+    }
+
+    @Test
+    public void itsNotTriangleTest(){
+        Triangle obj1= new Triangle(-3, 4, 5);
+        Assert.assertEquals(false, obj1.itsTriangle());
+    }
+
+    @Test
+    public void ColculateArea(){
+        Triangle obj1= new Triangle(3, 4, 5);
+        Assert.assertEquals(true, obj1.canColculateArea(obj1));
+    }
+
+    @Test
+    public void NotColculateArea(){
+        Triangle obj1= new Triangle(-3, 1, 5);
+        Assert.assertEquals(false, obj1.canColculateArea(obj1));
+    }
+
+}
